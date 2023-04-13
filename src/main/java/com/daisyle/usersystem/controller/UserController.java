80d5e8e356aae6fefffde4dd06cebbc29a1d41a8
Daisy Le <daisy.le0203@gmail.com>
2023-04-12 22:35:50 -0500

    add crud functionality to users end points

--- src/main/java/com/daisyle/usersystem/controller/UserController.java
+++ src/main/java/com/daisyle/usersystem/controller/UserController.java
@@ -1,17 +1,48 @@
 package com.daisyle.usersystem.controller;
 
+import com.daisyle.usersystem.model.User;
 import com.daisyle.usersystem.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+    @PostMapping("/users")
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User user = null;
+        user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable("id") Long id) {
+        boolean deleted = false;
+        deleted = userService.deleteUser(id);
+        Map <String,Boolean> response = new HashMap<>();
+        response.put("deleted", deleted);
+        return ResponseEntity.ok(response);
+    }
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id,
+                                           @RequestBody User user) {
+        user = userService.updateUser(id, user);
+        return ResponseEntity.ok(user);
     }
 }
